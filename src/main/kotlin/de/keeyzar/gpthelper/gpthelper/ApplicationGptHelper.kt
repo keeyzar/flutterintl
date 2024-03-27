fb1ec1a008653011c8d48b1844cc432639815bc5
Kevin Kekule <k.kekule@web.de>
2024-03-27 15:11:22 +0100

    check if koin was already started - get rid of errors

--- src/main/kotlin/de/keeyzar/gpthelper/gpthelper/ApplicationGptHelper.kt
+++ src/main/kotlin/de/keeyzar/gpthelper/gpthelper/ApplicationGptHelper.kt
@@ -4,17 +4,21 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import de.keeyzar.gpthelper.gpthelper.DIConfig.Companion.appModule
 import de.keeyzar.gpthelper.gpthelper.features.shared.presentation.Initializer
+import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
 
 
 class ApplicationGptHelper() : StartupActivity {
 
     override fun runActivity(project: Project) {
-        startKoin() {
-            modules(appModule)
+        //ensure koin is not started twice
+        if (GlobalContext.getOrNull() == null) {
+            startKoin {
+                modules(appModule)
+            }
+            val initializer = Initializer()
+            initializer.translationPercentageBus.init(project)
+            initializer.currentProjectProvider.project = project
         }
-        val initializer = Initializer()
-        initializer.translationPercentageBus.init(project)
-        initializer.currentProjectProvider.project = project
     }
 }
